480a95007f54b34c746331148e22db77a6acb4c2
@kavinduushan07 <kavinduushan07@gmail.com>
2024-11-24 16:28:04 +0530

    create search function and complete the project

--- ecom-project/src/main/java/com/ushan/ecom_project/repo/ProductRepo.java
+++ ecom-project/src/main/java/com/ushan/ecom_project/repo/ProductRepo.java
@@ -2,10 +2,19 @@ package com.ushan.ecom_project.repo;
 
 import com.ushan.ecom_project.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 
+//    use the JPQL for write the search function
+//    table names = class names | column names == fields name in JPQL
+    @Query("SELECT p from Product p WHERE "+
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(String keyword);
 
 }
