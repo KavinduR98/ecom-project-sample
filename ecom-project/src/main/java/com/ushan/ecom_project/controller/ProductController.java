c49e966613ae897a5b101b1d610a08a0b869fee1
@kavinduushan07 <kavinduushan07@gmail.com>
2024-11-24 10:27:54 +0530

    update controller, repo and Service

--- ecom-project/src/main/java/com/ushan/ecom_project/controller/ProductController.java
+++ ecom-project/src/main/java/com/ushan/ecom_project/controller/ProductController.java
@@ -1,15 +1,31 @@
 package com.ushan.ecom_project.controller;
 
+import com.ushan.ecom_project.model.Product;
+import com.ushan.ecom_project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
+
+    @Autowired
+    private ProductService service;
 
     @RequestMapping("/")
     public String greet(){
         return "Hello world";
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return service.getAllProducts();
     }
 
 }
